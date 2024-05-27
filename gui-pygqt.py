9402dbe6637b01ee53dc708d1669bd59aa25d63c
August <augustroosi@gmail.com>
2024-05-27 20:29:04 +0300

    Update gui-pygqt.py

--- gui-pygqt.py
+++ gui-pygqt.py
@@ -1,40 +1,45 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QWidget, QSpacerItem, QSizePolicy, QProgressBar, QLayout, QMessageBox
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt, QPropertyAnimation
+import time
+from PyQt5.QtWidgets import QApplication, QLabel, QSplashScreen, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QWidget, QSpacerItem, QSizePolicy, QProgressBar, QMessageBox
+from PyQt5.QtGui import QPixmap, QColor
 from PyQt5.QtWidgets import QGraphicsDropShadowEffect
-from PyQt5.QtGui import QColor
+from PyQt5.QtCore import Qt, QTimer
 from PIL import Image
 import tensorflow as tf
 import numpy as np
 
 
+class SplashScreen(QSplashScreen):
+    def __init__(self):
+        super().__init__(QPixmap("splashscreen.jpg"))  # Asendage "splash_screen.png" oma tegeliku splash screeni pildiga
+        self.showMessage("Laadin mudelit...", alignment=Qt.AlignBottom | Qt.AlignHCenter, color=Qt.white)
+
+
 class ImageProcessorApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.model = None
+        self.splash = SplashScreen()  # Looge splash screeni instants
+        self.splash.show()
+        self.load_model()
         self.initUI()
+        self.splash.finish(self)
         
 
     def initUI(self):
-        self.model = self.load_model()
+        
+        # Siin tekitame kõik komponendid 
         self.layout = QVBoxLayout()
-
-        self.layout.setSpacing(30)  # Vahe komponentide vahel
-
-
-
-        # Spacer enne kõiki komponente
+        
+        self.layout.setSpacing(30) 
         self.layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
-
-        # Sisemine paigutus, mis sisaldab kõiki komponente
+        
         inner_layout = QVBoxLayout()
-
+        
         self.image_label = QLabel()
         self.image_label.setAlignment(Qt.AlignCenter)
         inner_layout.addWidget(self.image_label)
         
-
-        # Horisontaalne paigutus tekstide jaoks
         self.prog_layout1 = QHBoxLayout()
         self.prog_layout2 = QHBoxLayout()
         self.prog_layout3 = QHBoxLayout()
@@ -42,10 +47,6 @@ class ImageProcessorApp(QWidget):
         self.prog_layout1.addStretch()
         self.prog_layout2.addStretch()
         self.prog_layout3.addStretch()
-
-
-
-        
         
         self.label1 = QLabel("")
         self.label1.setAlignment(Qt.AlignCenter)
@@ -74,7 +75,7 @@ class ImageProcessorApp(QWidget):
         self.progress_bar3.setStyleSheet("QProgressBar::chunk { background-color: rgba(0, 255, 0, 0.8); border: 1px solid gray; border-top-right-radius: 3px; border-bottom-right-radius: 3px; } QProgressBar {border: 1px solid gray; border-radius: 3px;}")
         self.prog_layout3.addWidget(self.progress_bar3)
         
-        width = 80
+        width = 100
         height= 20
         
         self.label1.setFixedSize(width, height)
@@ -91,7 +92,6 @@ class ImageProcessorApp(QWidget):
         self.prog_layout3.addStretch()
 
 
-        # Lisame horisontaalse paigutuse sisemisse paigutusse
         inner_layout.addLayout(self.prog_layout1)
         inner_layout.addLayout(self.prog_layout2)
         inner_layout.addLayout(self.prog_layout3)
@@ -112,58 +112,58 @@ class ImageProcessorApp(QWidget):
 
         self.question_button = QPushButton("?")
         self.question_button.clicked.connect(self.show_dialog)
-        self.question_button.setMaximumSize(30, 30)  # Maksimaalne suurus küsimärgiga nupule
-        self.question_button.setToolTip("Abi")  # Kuvab tööriistaipundi
-        self.question_button.setStyleSheet("QPushButton { font-size: 18px; }")  # Nupu stiil
+        self.question_button.setMaximumSize(30, 30)  
+        self.question_button.setToolTip("Abi")  
+        self.question_button.setStyleSheet("QPushButton { font-size: 18px; }")  
         inner_layout.addWidget(self.question_button, alignment=Qt.AlignRight)
 
-        # Lisa sisemine paigutus
         self.layout.addLayout(inner_layout)
 
-        # Spacer pärast kõiki komponente
         self.layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
         self.setLayout(self.layout)
         self.setWindowTitle("Hinda raamatut kaane järgi")
 
     def load_model(self):
-        # Asendage see funktsioon vastavalt teie mudeli laadimisele
-        model = tf.keras.models.load_model('face_ratings_model.h5')
-        return model
-    
+        self.model = tf.keras.models.load_model('face_ratings_model.h5')
+        
     
     def preprocess_image(self, image_path):
         image = Image.open(image_path)
         
-        #image = image.crop(coords_for_crop(image_path))
-        image = image.resize((600, 600))  # Ensure the image is 600x600 pixels
-        image.show()
-        image = np.array(image) / 255.0  # Normalize the image
-        image = np.expand_dims(image, axis=0)  # Add batch dimension
+        image = image.resize((600, 600))
+        image = np.array(image) / 255.0  
+        image = np.expand_dims(image, axis=0)  
         return image
 
     def ennusta(self, image_path):
 
-        # Preprocess the image
         preprocessed_image = self.preprocess_image(image_path)
-        
-        # Predict the ratings
         predicted_ratings = self.model.predict(preprocessed_image)
 
-        # Print the predicted ratings
         return predicted_ratings[0]
 
 
-
     def show_dialog(self):
-        # Hüpikakna kuvamine
         QMessageBox.information(self, "Abi", "See tehisintellekt on õpetatud hindama inimesi nii, kuidas teised neid näeks.")
+
+    def show_error(self):
+        QMessageBox.information(self, "Viga", "Selle pildi formaat ei sobi meie mudelile. :(")
+
 
     def open_image(self):
         options = QFileDialog.Options()
         options |= QFileDialog.ReadOnly
         file_path, _ = QFileDialog.getOpenFileName(self, "Vali pilt", "", "Image Files (*.jpg *.jpeg *.png)", options=options)
         if file_path:
+            
+            try:
+                vals = self.ennusta(file_path)
+            except:
+                self.show_error()
+                return
+            
+            
             image = Image.open(file_path)
             image.thumbnail((300, 300))
             image.save("thumbnail.png")
@@ -172,7 +172,6 @@ class ImageProcessorApp(QWidget):
             self.image_label.setPixmap(pixmap)
 
 
-            vals = self.ennusta(file_path)
             
             self.button.setText("Vali uus pilt")
             
@@ -184,12 +183,10 @@ class ImageProcessorApp(QWidget):
             self.progress_bar2.setValue(int(vals[1]*10))
             self.progress_bar3.setValue(int(vals[0]*10))
 
-            # Näita progressbare pärast töötlemist
             self.progress_bar1.show()
             self.progress_bar2.show()
             self.progress_bar3.show()
 
-            # Lisa varjuefekt ja animatsioonid pärast progressbaride näitamist
             effect = QGraphicsDropShadowEffect()
             effect.setBlurRadius(5)
             effect.setColor(QColor('gray'))
@@ -203,10 +200,10 @@ class ImageProcessorApp(QWidget):
             effect3.setColor(QColor('gray'))
             effect3.setOffset(3)
             
-
             self.progress_bar1.setGraphicsEffect(effect)
             self.progress_bar2.setGraphicsEffect(effect2)
             self.progress_bar3.setGraphicsEffect(effect3)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -214,3 +211,4 @@ if __name__ == '__main__':
     ex.resize(800, 600)
     ex.show()
     sys.exit(app.exec_())
+
